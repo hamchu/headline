bc4c9038ad0eece0020f6b31f54a47e03c26c337
hamchu <64580070+hamchu@users.noreply.github.com>
2020-10-07 20:39:00 +0900

    Add function for Crawler
    + encode image to base64string

---
+++ Utils.py
@@ -0,0 +1,8 @@
+from base64 import b64encode
+import urllib.request
+
+def encode_image_to_base64string(image_url):
+    with urllib.request.urlopen(image_url) as image_file:
+        s = b64encode(image_file.read())
+        encoded_string = s.decode("utf-8")
+    return encoded_string
