4f3f40162ef1c8098baded5edc2271a31ad5235b
hamchu <64580070+hamchu@users.noreply.github.com>
2020-10-11 18:58:23 +0900

    Update SentenceProcessor Class
    1. Add load stop_words.csv in __init__
    2. Add preprocess_sentence
    3. Add embed_sentence

--- SentenceProcessor.py
+++ SentenceProcessor.py
@@ -1,5 +1,6 @@
 from gensim import models
 from nltk import sent_tokenize, word_tokenize
+import pandas as pd
 
 class SentenceProcessor():
 
@@ -8,6 +9,29 @@ class SentenceProcessor():
         self.fasttext_model = models.fasttext.load_facebook_model('fasttext/cc.ko.300.bin')  # 300차원
         print("Load FastText Model - Done!")
 
+        print("Load stop_words.csv...")
+        self.stop_words = sum(pd.read_csv("data/stop_words.csv").values.tolist(),[])
+        print("Load stop_words - Done!")
+
     def tokenize_sentence(self, sentences):
-        tokenized_sentences = sent_tokenize(sentences);
+        tokenized_sentences = sent_tokenize(sentences)
+
         return [word_tokenize(sentence) for sentence in tokenized_sentences]
+
+    def preprocess_sentence(self, sentences):
+        preprocessed_sentences = []
+
+        for sentence in sentences:
+            temp = [word for word in sentence if word not in self.stop_words and word]
+            preprocessed_sentences.append(temp)
+
+        return preprocessed_sentences
+
+    def embed_sentence(self, sentences):
+        embedded_sentences = []
+
+        for sentence in sentences:
+            temp = sum([self.fasttext_model.wv.__getitem__(word) for word in sentence]) / len(sentence)
+            embedded_sentences.append(temp)
+
+        return embedded_sentences
