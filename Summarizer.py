d03e2f26e100edb939bec6be3208112ad4b4b42a
hamchu <64580070+hamchu@users.noreply.github.com>
2020-10-12 18:07:51 +0900

    Add Summarizer Class

---
+++ Summarizer.py
@@ -0,0 +1,39 @@
+from SentenceProcessor import SentenceProcessor
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import networkx as nx
+
+class Summarizer():
+    def __init__(self):
+        print("Summarizer Initializing...")
+        self.sentence_processor = SentenceProcessor()
+        print("Summarizer - Done!")
+
+    def calculate_similarity_matrix(self, embedded_sentences):
+        similarity_matrix = np.zeros([len(embedded_sentences), len(embedded_sentences)])
+
+        for i in range(len(embedded_sentences)):
+            for j in range(len(embedded_sentences)):
+                similarity_matrix[i][j] = cosine_similarity(embedded_sentences[i].reshape(1, 300),
+                                                  embedded_sentences[j].reshape(1, 300))[0, 0]
+
+        return similarity_matrix
+
+    def get_score(self, similarity_matrix):
+        nx_graph = nx.from_numpy_array(similarity_matrix)
+        scores = nx.pagerank(nx_graph)
+        return scores
+
+    def summarize(self, text):
+        tokenized_sentences = self.sentence_processor.tokenize_sentence(text)
+        preprocessed_sentences = self.sentence_processor.preprocess_sentence(tokenized_sentences)
+        embedded_sentences = self.sentence_processor.embed_sentence(preprocessed_sentences)
+        similarity_matrix = self.calculate_similarity_matrix(embedded_sentences)
+        scores = self.get_score(similarity_matrix)
+
+        score_ranking_list = sorted(((scores[i], s) for i, s in enumerate(self.sentence_processor.get_tokenized_sentences(text))),
+                            reverse=True)
+        top_sentences = [sentence for score, sentence in score_ranking_list[:3]]
+        summary = " ".join(top_sentences)
+
+        return summary
