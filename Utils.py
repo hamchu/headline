02d0a0ff9c2e6bedf1238b4ceea88c2d0625990f
hamchu <64580070+hamchu@users.noreply.github.com>
2020-10-16 20:55:32 +0900

    Refactor image to base64 function
    - Use openCV for reducing image size.

--- Utils.py
+++ Utils.py
@@ -1,8 +1,12 @@
 from base64 import b64encode
-import urllib.request
+import urllib.request, cv2
+import numpy as np
 
 def encode_image_to_base64string(image_url):
-    with urllib.request.urlopen(image_url) as image_file:
-        s = b64encode(image_file.read())
-        encoded_string = s.decode("utf-8")
+    req = urllib.request.urlopen(image_url)
+    image = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    image = cv2.imdecode(image, -1)
+    resized_image = cv2.resize(image, dsize=(0, 0), fx=0.2, fy=0.2, interpolation=cv2.INTER_AREA)
+    s = b64encode(resized_image.tobytes())
+    encoded_string = s.decode("utf-8")
     return encoded_string
